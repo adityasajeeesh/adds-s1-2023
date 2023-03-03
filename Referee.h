bee9be12445307dc831d5cc2f056027b027c564e
adityasajeeesh <110010159+adityasajeeesh@users.noreply.github.com>
2023-03-03 12:44:37 +1030

    Added default constructor

--- Referee.h
+++ Referee.h
@@ -8,7 +8,7 @@
 
 class Referee {
     public:
-        Referee(); // constructor
+        Referee(){}; // constructor
         Player * refGame(Player * player1, Player * player2){}; // returns the reference to the winning player
 };
 
