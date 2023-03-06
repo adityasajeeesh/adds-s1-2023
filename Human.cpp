d0b31442ea20502131c87fc000f71f52a822cbe5
adityasajeeesh <110010159+adityasajeeesh@users.noreply.github.com>
2023-03-06 14:01:59 +1030

    Removed return playerMove from makeMove()

--- Human.cpp
+++ Human.cpp
@@ -8,7 +8,6 @@ std::string Human::getName(){
 char Human::makeMove(){
     std::cout << "Enter move: ";
     std::cin >> playerMove;
-    return playerMove;
 };
 char Human::getMove(){
     return playerMove;
