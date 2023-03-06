5592cd6d94bd3ce849c168482db053e5978aa55d
adityasajeeesh <110010159+adityasajeeesh@users.noreply.github.com>
2023-03-06 11:24:27 +1030

    Manipulation of code to potentially work

--- Referee.cpp
+++ Referee.cpp
@@ -5,20 +5,20 @@
 #include "Referee.h"
 #include<string>
 
-Player * Referee::refGame(Player * player1, Player * player2){
-    Player * winner;
+int Referee::refGame(Player * player1, Player * player2){
+    int winner;
     player1->makeMove();
     player2->makeMove();
     if(player1->makeMove() == 'P' && player2->makeMove() == 'R' ||
         player1->makeMove() == 'S' && player2->makeMove() == 'P' ||
         player1->makeMove() == 'R' && player2->makeMove() == 'S'){
-        winner = player1;
+        winner = 1;
     }
     else if(player1->makeMove() == player2->makeMove()){
-        winner = nullptr;
+        winner = 0;
     }
     else{
-        winner = player2;
+        winner = 2;
     }
     return winner;
 };
